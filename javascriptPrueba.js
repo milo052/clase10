504b11b8965f67796f6790af531c5015df5fb601
milo052 <130940713+milo052@users.noreply.github.com>
2023-05-23 19:20:10 -0600

    clase10
    esta es la clase 10

---
+++ javascriptPrueba.js
@@ -0,0 +1,18 @@
+var Nombre = "Milo";
+var Apellido = "Arevalo";
+
+console.log(Nombre+" "+Apellido );
+
+
+
+var numero1= 10;
+var   numero2=  15;
+var suma = numero1 + numero2;
+
+console.log(suma);
+
+var numero3= suma + 20;
+
+console.log(numero3);
+
+
